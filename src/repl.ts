75bac9ccf78545d25fec2be810207dd8b3a1a923
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-05-06 07:16:21 -0700

    add repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,27 @@
+import { createInterface } from "readline";
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (!words.length) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+    console.log(`Your command was: ${commandName}`);
+    rl.prompt();
+  });
+}
+
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
@@ -5,3 +29,4 @@ export function cleanInput(input: string): string[] {
     .split(" ")
     .filter((word) => word !== "");
 }
+
