d4a75569da225cc4e1c197dc7fd618868dbeb1e0
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-05-07 00:09:53 -0700

    add pokedex command

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -3,6 +3,7 @@ import type { State } from "./state.js";
 export async function commandExit(state: State) {
   console.log("Closing the Pokedex... Goodbye!");
   state.readline.close();
+  state.pokeAPI.closeCache();
   process.exit(0);
 }
 
